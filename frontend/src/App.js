2bb2111a9893a6fe8ed9c2ac2d5ed01393547342
Jacob-Mahloko <114677637+Jacob-Mahloko@users.noreply.github.com>
2023-12-30 01:12:33 +0200

    ✅ 💄intial login functionality using states

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import NavBar from './components/navbar/navbar';
 import Footer from './components/footer/footer';
-import Files from './components/files/files';
 import Login from './components/login/login';
+
+
 function App() {
   //<Files fName='Media' Ftype='photo' date='2023-12-29'/>
   return (
     <div className="App">
       <NavBar />
-      
+      <Login/>
       <Footer />
 
     </div>
--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css'
 
 export default function Login() {
+  const username="Jacob123";
+  const password="password";
+
+  const [usernameState,setUsernameState]=useState('');
+  const [passwordState,setPasswordState]=useState('');
+  const[loggedIn,setLoggin]=useState(false);
+
+  function loggin(){
+    if(usernameState===username && passwordState===password){
+      setLoggin(true);
+    }
+  }
+
+  const popUp=()=>{
+    alert('logged in');
+  }
   return (
     
     <div className='LoginForm'>
         <label>username: </label>
-        <input type='text' />
+        <input type='text' onChange={(event)=>{setUsernameState(event.target.value)}}/>
         <label>password: </label>
-        <input type='password' />
-        <button>Login</button>
+        <input type='password' onChange={(event)=>{setPasswordState(event.target.value)}}/>
+        <button onClick={loggin}>Login</button>
+        {loggedIn && popUp()}
     </div>
-  )
+    
+  );
 }
