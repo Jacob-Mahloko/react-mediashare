bc414f4dc0a12caca2ceba63454fd9eb945eec77
Jacob-Mahloko <114677637+Jacob-Mahloko@users.noreply.github.com>
2024-01-05 20:28:25 +0200

    dealing with arrays

--- frontend/src/components/files/files.js
+++ frontend/src/components/files/files.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
 import './files.css'
-export default function Files({fName,Ftype,date}) {
+export default function Files(/*{fName,Ftype,date}*/) {
+  
+  const [item,itemFunction] = useState()
+  const [state,stateFunction] = useState([])
   return (
     <div className='Files'>
-        <hr/>
+        {/*<hr/>
         <h4>Filename:{fName}</h4>
         <h4>Type of files: {Ftype}</h4>
         <h4>Date uploaded: {date}</h4>
-        <hr/>
+        <hr/>*/}
+        <input type='text' onChange={(event)=>{
+          itemFunction(event.target.value)
+          console.log(item)}}/>
+        <button type='submit' onClick={()=>{
+          stateFunction([...state,item])
+          console.log(state)
+          itemFunction('')
+        }} >Add to Cart</button>
+
+        {state.map((value,key)=>{
+          return <h2 key={key}>{value}</h2>
+        })}
     </div>
+
+    
   )
 }
