8bfc5bdcb2e33b8f7668a49c5439389ba29e8342
ShiluveloMakhubel <117033636+ShiluveloMakhubel@users.noreply.github.com>
2024-01-29 17:48:47 +0200

    naviagtion update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import NavBar from './components/navbar/navbar';
 import Footer from './components/footer/footer';
-//import Login from './components/login/login';
+import Login from './components/login/login';
 import Files from './components/files/files';
+import React, { useState } from 'react';
 
 function App() {
   //<Files fName='Media' Ftype='photo' date='2023-12-29'/>
+  const [currentPage, setCurrentPage] = useState('home');
+
   return (
     <div className="App">
-      <NavBar />
-      <Files/>
+      <NavBar setPage={setCurrentPage} />
+      
+      {currentPage === 'home' && <h2>Home</h2>}
+      {currentPage === 'login' && <Login />}
+      {currentPage === 'about' && <h2>About</h2>}
+      {currentPage === 'contact' && <h2>Contact</h2>}
+
+      
       <Footer />
     </div>
   );
--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React , { useState }from 'react'
 import './navbar.css'
 import login from '../login/login';
-export default function NavBar() {
+export default function NavBar({ setPage }) {
   return (
     <div className='navbar'>
         <ul>
-            <li>
-                <a href=''>Home</a>
-            </li>
-            <li>
-                <a href=''>Login</a>
-            </li>
-            <li>
-                <a href=''>About</a>
-            </li>
-            <li>
-                <a href=''>Contact</a>
-            </li>
-        </ul>
+        <li>
+          <a href='#' onClick={() => setPage('home')}>Home</a>
+        </li>
+        <li>
+          <a href='#' onClick={() => setPage('login')}>Login</a>
+        </li>
+        <li>
+          <a href='#' onClick={() => setPage('about')}>About</a>
+        </li>
+        <li>
+          <a href='#' onClick={() => setPage('contact')}>Contact</a>
+        </li>
+      </ul>
     </div>
   );
 }
